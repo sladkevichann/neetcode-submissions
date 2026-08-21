3dfb729f8204275b730080e4567a3d323a3e67e3
sladkevichann <154851914+sladkevichann@users.noreply.github.com>
2026-08-21 19:13:19 -0400

    Add: three-integer-sum - submission-5

---
+++ Data Structures & Algorithms/three-integer-sum/submission-5.py
@@ -0,0 +1,28 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        # 2 pointers solution
+        
+        nums.sort() # complexity nlog(n) - for duplicates
+        res = []
+
+        for i in range(len(nums)):
+            if i and nums[i] == nums[i -1]:
+                continue
+
+            j, k = i + 1, len(nums) - 1 # high aura definition
+
+            while j < k:
+                if nums[i] + nums[j] + nums[k] < 0:
+                    j += 1
+                elif nums[i] + nums[j] + nums[k] > 0:
+                    k -= 1
+                elif nums[i] + nums[j] + nums[k] == 0:
+                    res.append([nums[i], nums[j], nums[k]])
+                    j += 1
+                    k -= 1
+                    while j < k and nums[j - 1] == nums[j]: # дубликаты. можно было и k двигать вниз с таким же успехом
+                        j += 1
+        return res
+
+
+        
